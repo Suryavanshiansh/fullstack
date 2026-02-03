49feb8b8e7f056e45fd6644b67f4f9a942c3d4c6
Ansh_Suryavanshi <suryavanshiansh2007@gmail.com>
2026-02-03 10:04:18 +0530

    Add files via upload

---
+++ DOM/script.js
@@ -0,0 +1,9 @@
+console.log("Hello, DOM!");
+let heading = document.querySelector("#heading")
+console.dir(heading)
+let firstElement = document.querySelector(".paragraph")
+console.dir(firstElement)
+let para = document.querySelectorAll(".paragraph")
+console.log(para)
+let tag = document.querySelectorAll("p")
+console.log(tag)
